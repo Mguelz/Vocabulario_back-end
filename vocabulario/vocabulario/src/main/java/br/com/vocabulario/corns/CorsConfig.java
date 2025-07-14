22b2ea4fb44c4ca565afa50f807a1e9a355abfbf
Miguel <945507756m@gmail.com>
2025-07-14 16:55:08 -0300

    add banco na nuvem

---
+++ vocabulario/vocabulario/src/main/java/br/com/vocabulario/corns/CorsConfig.java
@@ -0,0 +1,22 @@
+package br.com.vocabulario.corns;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") // libera todas as rotas
+						.allowedOrigins("http://localhost:3000") // origem do React
+						.allowedMethods("GET", "POST", "PUT", "DELETE");
+			}
+		};
+	}
+
+}
