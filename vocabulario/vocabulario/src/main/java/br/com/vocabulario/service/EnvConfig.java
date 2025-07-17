a9651940d1158c381bd7f3b772bc94aa562fccc1
Miguel <945507756m@gmail.com>
2025-07-17 14:58:14 -0300

    remove dotenv-java - erro no deplay render

--- vocabulario/vocabulario/src/main/java/br/com/vocabulario/service/EnvConfig.java
+++ vocabulario/vocabulario/src/main/java/br/com/vocabulario/service/EnvConfig.java
@@ -1,16 +1,16 @@
-package br.com.vocabulario.service;
-
-import org.springframework.context.annotation.Configuration;
-
-import io.github.cdimascio.dotenv.Dotenv;
-
-
-@Configuration
-public class EnvConfig {
-	static {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
-	}
-}
+//package br.com.vocabulario.service;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//
+//
+//@Configuration
+//public class EnvConfig {
+//	static {
+//		Dotenv dotenv = Dotenv.load();
+//		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+//		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+//		System.setProperty("DB_PASS", dotenv.get("DB_PASS"));
+//	}
+//}
